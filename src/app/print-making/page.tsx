121dd85c017d0a83f532aa79deccc71d8c5312c3
Alt5r <rrow8nn@gmail.com>
2024-12-30 12:47:52 +0000

    padding on printmaking carusel

--- src/app/print-making/page.tsx
+++ src/app/print-making/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div style={{ backgroundColor: '#bcc5d1' }}>
       <Navbar />
-      <div className="flex justify-center min-h-screen" style={{ backgroundColor: '#bcc5d1' }}>
+      <div className="flex justify-center min-h-screen p-3" style={{ backgroundColor: '#bcc5d1' }}>
       <div className="w-3/4 max-w-2xl max-h-[70vh]" style={{ backgroundColor: '#bcc5d1' }}>
         <Carusel />
         
